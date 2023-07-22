3fe34f249021432aaa3cab01bf8eb8c58493889b
saifullahemo <saifullahemo@gmail.com>
2023-07-22 11:06:20 +0600

    -- product created

--- cypress/e2e/Products.cy.js
+++ cypress/e2e/Products.cy.js
@@ -1,4 +1,6 @@
 import './commands';
+import 'cypress-file-upload';
+
 import categoriesData from '../fixtures/products.json';
 
 describe('User login', () => {
@@ -30,37 +32,64 @@ describe('User login', () => {
   const maxNumber = 9999999999; // Maximum number (inclusive)
   const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;    
 
+  // + Random category selection functionality  
   function selectRandomCategory() {
     const categories = categoriesData.categories;
     const randomIndex = Math.floor(Math.random() * categories.length);
     return categories[randomIndex];
   }
+
+  // + Random units selection functionality  
+  function selectRandomUnits() {
+    const units = categoriesData.units;
+    const randomIndex = Math.floor(Math.random() * units.length);
+    return units[randomIndex];
+  }
+
+  // + Random Brands selection functionality  
+  function selectRandomBrands() {
+    const brands = categoriesData.brands;
+    const randomIndex = Math.floor(Math.random() * brands.length);
+    return brands[randomIndex];
+  }
+
+  // + Random products selection functionality  
+  function selectRandomProducts() {
+    const products = categoriesData.products;
+    const randomIndex = Math.floor(Math.random() * products.length);
+    return products[randomIndex];
+  }
+
+
   
   it('should be created successfully', () => {
 
     // Generate a random string to append to the base email
-    const randomString = Math.random().toString(36).substring(7);
+    const randomStrings = Math.random().toString(36).substring(7);
     // Base email address
-    const baseEmail = 'testinguser@test.com';
+    const baseEmail = 'test@gmail.com';
     // Unique email by appending random string
-    const email = `${randomString}${baseEmail}`;
+    const email = `${randomString} ${baseEmail}`;
     // Unique email by appending random string
     const name = "Hair pin";
     // const name = `${baseStore} ${randomString}`;
     // const mobileNumber = '01712343434';
     const mobileNumber = `1 (${randomNumber.toString().substr(1, 3)}) ${randomNumber.toString().substr(4, 3)}-${randomNumber.toString().substr(7, 4)}`;
 
-    const fax = '125548';
+    const skuNumber = `${randomStrings}`;
+    const barcode = `${randomString}`;
     const addressLine1 = '1181/4, Gulshan-2, Dhaka';
     const suburb = 'Gulshan';
     const state = 'Dhaka';
     const postCode = '1201';
     const country = 'Bangladesh';
     const tax = '120012';
-    const website = 'google.com';
-    // const warehouseName = 'Hobart'
-    // const warehouseName = Cypress.env('warehouseName');
-    // const categories = categoriesData.categories;      
+    const website = 'google.com';    
+    const category = selectRandomCategory();
+    const unit = selectRandomUnits();
+    const brand = selectRandomBrands();
+    const product = selectRandomBrands();
+    const fileName = 'bank.jpg';
 
 
     cy.visit('/');
@@ -72,18 +101,64 @@ describe('User login', () => {
     cy.contains(' :nth-child(4) > .d-flex > .menu-title', 'Categories').click();
     cy.contains('.col-md-8 > .d-flex > .btn', 'Add Category').click();
 
-    // const randomCategoryIndex = Math.floor(Math.random() * categories.length);
-    // const selectedCategory = categories[randomCategoryIndex];
-
-    // const randomCategoryIndex = Math.floor(Math.random() * categories.length);
-    // const selectedCategory = categories[randomCategoryIndex].name;
-    const category = selectRandomCategory();
     cy.get('#category-name').type(category);
-
-    //cy.get('#category-name').type(selectedCategory);
+    
     cy.get('#category-short-name').type("NTL");
     cy.contains('.mr-2', 'Add').click()
+    
+    //units creation
+    cy.contains(':nth-child(3) > .d-flex','Units').click()
+    cy.wait(3000)
+    cy.contains('.col-md-8 > .d-flex > .btn', 'Add Unit').click();
+    cy.get('#autosuggest').type(unit);
+    cy.contains('#add-new-unit-sidebar > .b-sidebar-body > :nth-child(2) > .p-2 > .mt-2 > .mr-2', 'Add').click();
+    
+    //  Brands creation
+    cy.contains(':nth-child(2)', 'Brands').click();
+    cy.contains('.col-md-8 > .d-flex > .btn', 'Add Brand').click();
+    cy.get('#brand-name').type(brand);
+    cy.contains('#add-new-brand-sidebar > .b-sidebar-body > :nth-child(2) > .p-2 > .mt-2 > .mr-2', 'Add').click()
+    cy.wait(3000)
 
+    //Products list creation
+    cy.contains(':nth-child(1)', 'Product List').click();
+    cy.contains('.col-md-8 > .d-flex > .btn', 'Add Product').click();
+    cy.get('#sku-number').type(skuNumber)
+    cy.get('#product-name').type(product);
+    cy.get('#product-barcode').type(barcode);
+    cy.get('#vs7__combobox').click();
+    selectRandomStore();
+    
+    cy.get('#vs8__combobox').click();
+    selectRandomStore();
+    
+    cy.get('#vs9__combobox').click();
+    selectRandomStore();
+    
+    cy.get('#vs10__combobox').click();
+    selectRandomStore();
+    
+    cy.get('#vs11__combobox').click();
+    selectRandomStore();
+
+
+      // Upload file
+      cy.contains('#add-new-product-sidebar > .b-sidebar-body > :nth-child(2) > .px-2 > :nth-child(9) > .col > .media > .media-body > .d-flex > .btn-primary', 'Upload').invoke('removeAttr', 'style');
+      cy.fixture(fileName, 'binary').then(fileContent => {
+        cy.get('.btn').attachFile({
+          fileContent,
+          fileName,
+          mimeType: 'image/jpeg'
+        });
+      });
+  
+      // Wait for a reasonable amount of time for the file to be uploaded
+      cy.wait(5000);
+  
+      // Add user
+      cy.contains('.btn-primary', 'Add').click();
+      cy.wait(5000);
+      
 
   });
 });
